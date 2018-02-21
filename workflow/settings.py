3aa412cc97bf96ce7239ea69bd455898c84a3501
mvanasten <michelvanasten@yahoo.fr>
2018-02-21 16:14:35 +0100

    Add files via upload

--- workflow/settings.py
+++ workflow/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^*d+r9hgcc_s+^-(h91+&=z_l&n!5@vov$am3e_yaz@ax#a4fy'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
